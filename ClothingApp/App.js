10ba3812464cb3b213862f4b4463eb2be0c9c190
rachelkeenan <rkeenan2@villanova.edu>
2023-04-24 16:30:56 -0400

    Started all pages

--- ClothingApp/App.js
+++ ClothingApp/App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Home from './Home';
+import ItemsForSale from './ItemsForSale';
+import PostAnItem from './PostAnItem';
+import MyCart from './MyCart';
+import MyHistory from './MyHistory';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    //Use the NavigationContainer and the Stack to display your app
+    //<Text style={{textAlign: 'center', paddingTop: 300}}>Homework 5</Text>
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName='Home'>
+    <Stack.Screen name='Home' component={Home} />
+    <Stack.Screen name='Items For Sale' component={ItemsForSale} />  
+    <Stack.Screen name='Post an Item' component={PostAnItem} />
+    <Stack.Screen name='My Cart' component={MyCart} />
+    <Stack.Screen name='My History' component={MyHistory} />
+    </Stack.Navigator>    
+    </NavigationContainer>  
   );
 }
 
---
+++ ClothingApp/Home.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+
+const Home = () => {
+    //dataSource contains the data we want rendered as a list
+    //the dataSource should contain a unique key for each item in the array
+
+    const dataSource = [
+        {key: '001', tab: 'Items for Sale', description: 'friend'},
+        {key: '002', tab: 'Post an Item', description: 'friend'},
+        {key: '003', tab: 'My Cart'},
+        {key: '004', tab: 'Previous Posts'}
+      ];
+    
+      //the renderItem prop takes a function. The function has a parameter which
+      //is an item in the data source. Place the item in a component to display the item.
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={dataSource}
+        renderItem={({item}) =>    
+        <TouchableOpacity onPress={console.log("Pressed")}>    
+        <View style={styles.border}>
+        <Text style={styles.item}>{item.tab}</Text>   
+        <Text style={styles.item}>{item.description}</Text>  
+        </View> 
+        </TouchableOpacity> 
+        }
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+     flex: 1,
+     paddingTop: 50
+    },
+    item: {
+      padding: 10,
+      fontSize: 18,
+      height: 44
+    },
+    border: {
+      borderWidth: 1,
+      sborderColor: 'gray'
+    }
+  });  
+
+export default Home;
