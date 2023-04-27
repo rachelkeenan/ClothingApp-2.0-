a0c9543f97e5660de7fec4bfeb1d64780e1c1ba4
caitlinvangoeverden <118017115+caitlinvangoeverden@users.noreply.github.com>
2023-04-26 20:25:21 -0400

    MyHistory

--- ClothingApp/MyHistory.js
+++ ClothingApp/MyHistory.js
@@ -1,110 +1,84 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity, Image } from 'react-native';
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
-export default function MyHistory({ navigation }) {
-  const [items, setItems] = useState([
-    {
-      id: '1',
-      name: 'Villanova Hoodie',
-      size: 'Medium',
-     // image: require('./assets/item1.jpg'),
-      price: 49.99,
-      description: 'Navy blue hoodie with white embroidered "Villanova" on the front. Made by Champion',
-      caption: 'Sold!'
-    },
-    {
-      id: '2',
-      name: 'Aritzia Gold Dress',
-      size: 'Small',
-     // image: require('./assets/item2.jpg'),
-      price: 59.99,
-      description: 'Satin gold mini dress with adjustable spagetti straps',
-      caption: 'Sold!'
-    },
-    {
-      id: '3',
-      name: 'New Balance',
-      size: '7.5',
-      //image: require('./assets/item3.jpg'),
-      price: 79.99,
-      description: 'Dark grey New Balance sneakers, never been worn!',
-      caption: 'For Sale'
-    },
-    {
-      id: '4',
-      name: 'Light Blue Madewell Jeans',
-      size: '25',
-      image: require('sp-pgm-dc-11.jpg'),
-      price: 69.99,
-      description: 'Straight leg light blue jeans.',
-      caption: 'Sold'
-    },
-  ]);
+const items = [
+  {
+    id: 1,
+    picture: 'https://images.sidearmdev.com/resize?url=https%3a%2f%2fdxbhsrqyrr690.cloudfront.net%2fsidearm.nextgen.sites%2fvillanova.com%2fimages%2f2022%2f8%2f15%2fAnastasia_Galanou.JPG&width=300&type=jpeg',
+    price: 10.99,
+    size: 'M',
+    description: 'Lorem ipsum dolor sit amet',
+    status: 'In Stock'
+  },
+  {
+    id: 2,
+    picture: 'https://example.com/item2.jpg',
+    price: 15.99,
+    size: 'L',
+    description: 'Consectetur adipiscing elit',
+    status: 'Out of Stock'
+  },
+  {
+    id: 3,
+    picture: 'https://example.com/item3.jpg',
+    price: 12.99,
+    size: 'S',
+    description: 'Sed do eiusmod tempor incididunt',
+    status: 'In Stock'
+  }
+];
 
-  const renderItem = ({ item }) => (
-    <TouchableOpacity style={styles.itemContainer} onPress={() => navigation.navigate('itemDetails')}>
-      <Image source={item.image} style={styles.itemImage} />
-      <Text style={styles.itemName}>{item.name}</Text>
-      <Text style={styles.itemSize}>{item.size}</Text>
-      <Text style={styles.itemPrice}>${item.price.toFixed(2)}</Text>
-      <Text style={styles.itemDescription}>{item.description}</Text>
-      <Text style={styles.itemCaption}>{item.caption}</Text>
-    </TouchableOpacity>
-  );
-
+const MyHistory = () => {
   return (
     <View style={styles.container}>
-      <FlatList
-        data={items}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-        numColumns={2}
-      />
+      {items.map(item => (
+        <View key={item.id} style={styles.item}>
+          <Image source={{ uri: item.picture }} style={styles.image} />
+          <View style={styles.details}>
+            <Text style={styles.price}>${item.price}</Text>
+            <Text style={styles.size}>{item.size}</Text>
+            <Text style={styles.description}>{item.description}</Text>
+            <Text style={styles.status}>{item.status}</Text>
+          </View>
+        </View>
+      ))}
     </View>
   );
-}
-
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 20
+  },
+  item: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc'
   },
-  itemContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: 10,
+  image: {
+    width: 100,
+    height: 100,
+    marginRight: 10
   },
-  itemImage: {
-    width: 150,
-    height: 150,
-    borderRadius: 10,
+  details: {
+    flex: 1
   },
-  itemName: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginTop: 10,
+  price: {
+    fontWeight: 'bold'
   },
-  itemPrice: {
-    fontSize: 16,
-    fontWeight: 'bold',
+  size: {
+    marginTop: 5
+  },
+  description: {
+    marginTop: 5
+  },
+  status: {
     marginTop: 5,
-  },
-  itemSize: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginTop: 5,
-  },
-  itemDescription: {
-    fontSize: 14,
-    marginTop: 5,
-  },
-  itemCaption: {
-    fontSize: 12,
-    color: 'gray',
-    marginTop: 5,
-  },
+    color: '#888'
+  }
 });
+
+export default MyHistory;
